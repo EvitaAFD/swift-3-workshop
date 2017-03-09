e59662276256e6e6c529b20acf722723e7d9fe11
EVE DENISON <evedenison@gmail.com>
2017-03-08 23:39:07 -0800

    tuples, arrays, strings

--- CFS3ToDoList/Swift3Workshop.playground/Contents.swift
+++ CFS3ToDoList/Swift3Workshop.playground/Contents.swift
@@ -9,34 +9,63 @@ myVariable = 50
 
 let myConstant = 42
 
-let myName = "Eve"
 
 //Numeric Type
 
+let minValue = UInt8.min
+let maxValue = UInt8.max
 
+let myFloat : Float = 1.234937
+let myDouble : Double = 1.234937987231234
 
 //String
 
+let firstName = "Eve"
+let lastName = "Denison"
+
+var fullName : String
+
+fullName = firstName + " " + lastName
 
 
 //Type Inference
 
-
+let myName = "Eve"
 
 //Tuple
 
+let tuple = (description: "Not Found", code: 401)
 
+tuple.description
 
 //Set
 
+let set : Set = [1, 2, 3, 4]
 
+let setB : Set = [1,9,8,4,2,8]
+
+let result = set.intersection(setB)
 
 //Array
 
+let stringsArray = [String]()
+let planetArray = ["Mars", "Jupiter"]
+
+var intArray = [Int](repeating: 0, count: 5)
+intArray.append(100)
+intArray.append(10)
+intArray.insert(1000, at: 0)
+
+let removedValue = intArray.remove(at: 7)
 
 
 //Dictionary
 
+let dictionaryOne = [String : String]()
+var dictionaryTwo = ["name" : "Mars"]
+
+dictionaryTwo["location"] = "Home"
+dictionaryTwo["name"] = "Earth"
 
 
 //For-In Loop
