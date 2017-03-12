7194ca76062eeecc201c58c2e309d0383bca6d08
EVE DENISON <evedenison@gmail.com>
2017-03-11 22:49:41 -0800

    completed first half of halb again

---
+++ CFS3ToDoList/NewTodoViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NewTodoViewController.swift
+//  CFS3ToDoList
+//
+//  Created by Eve Denison on 3/11/17.
+//  Copyright © 2017 Adam Wallraff. All rights reserved.
+//
+
+import UIKit
+
+class NewTodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
